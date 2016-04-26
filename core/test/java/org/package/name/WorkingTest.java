2987b7d92a8282d0ced64cc9c24b6cf1db082be9
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-04-26 09:28:12 +0200

    Removed video player + Trying unit tests

---
+++ core/test/java/org/package/name/WorkingTest.java
@@ -0,0 +1,20 @@
+package org.package.name;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class WorkingTest 
+{
+    @Test
+    public void thisAlwaysPasses() 
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() 
+    {
+    }
+}
