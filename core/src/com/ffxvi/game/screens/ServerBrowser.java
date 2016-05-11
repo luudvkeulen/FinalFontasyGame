2dfd25bc5e93c3dbc0badf45056aa8040956a9c3
Schilders,Koen K.N.D <koen.schilders@student.fontys.nl>
2016-05-11 13:19:23 +0200

    Init

---
+++ core/src/com/ffxvi/game/screens/ServerBrowser.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ffxvi.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.ffxvi.game.MainClass;
+import com.ffxvi.game.entities.PlayerCharacter;
+
+/**
+ *
+ * @author Koen
+ */
+public class ServerBrowser implements Screen {
+	
+	private MainClass game;
+	
+	public ServerBrowser(final MainClass game) {
+		this.game = game;
+	}
+
+	@Override
+	public void show() {}
+
+	@Override
+	public void render(float delta) {}
+
+	@Override
+	public void resize(int width, int height) {}
+
+	@Override
+	public void pause() {}
+
+	@Override
+	public void resume() {}
+
+	@Override
+	public void hide() {}
+
+	@Override
+	public void dispose() {}
+	
+}
