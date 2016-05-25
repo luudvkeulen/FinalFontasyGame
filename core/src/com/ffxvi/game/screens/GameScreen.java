ef872555559694d75b72b94ed63ffd012af259b9
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-05-25 09:41:29 +0200

    FIXED IP ADDRESS

--- core/src/com/ffxvi/game/screens/GameScreen.java
+++ core/src/com/ffxvi/game/screens/GameScreen.java
@@ -77,7 +77,7 @@ public class GameScreen implements Screen, Observer {
 		this.stage = new Stage();
 		this.chatManager = new ChatManager();
 		
-		this.client = new Client("localhost", 1338, 1337, this);
+		this.client = new Client("192.168.1.1", 1338, 1337, this);
 
 		//Setup map stuff
 		this.maps = new ArrayList();
