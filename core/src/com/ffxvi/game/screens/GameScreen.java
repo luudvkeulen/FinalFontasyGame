68c42ffd81e4e51f8ea2f780cf5bb3e1bfdcb22e
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-05-25 09:40:58 +0200

    Maybe fixed

--- core/src/com/ffxvi/game/screens/GameScreen.java
+++ core/src/com/ffxvi/game/screens/GameScreen.java
@@ -247,11 +247,13 @@ public class GameScreen implements Screen, Observer {
 			//Render other players
 			for(SimplePlayer splayer : multiplayers) {
 				ShapeRenderer srenderer = new ShapeRenderer();
+				srenderer.setProjectionMatrix(camera.combined);
 				srenderer.setAutoShapeType(true);
 				srenderer.begin();
 				//srenderer.circle((splayer.getX()/mainPlayer.getX()) + Gdx.graphics.getWidth()/2, (splayer.getY()/mainPlayer.getY()) + Gdx.graphics.getHeight()/2, 10);
-				Vector3 v3 = camera.unproject(new Vector3(splayer.getX(), splayer.getY(), 0));
-				srenderer.circle(v3.x, v3.y, 10);
+				//Vector3 v3 = camera.unproject(new Vector3(splayer.getX(), splayer.getY(), 0));
+				//srenderer.circle(v3.x, v3.y, 10);
+				srenderer.circle(splayer.getX(), splayer.getY(), 10);
 				srenderer.end();
 			}
 			
