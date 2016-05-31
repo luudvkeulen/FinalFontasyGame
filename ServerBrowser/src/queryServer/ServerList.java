32669dd5490b1d37ab23e14bdc9d21fe1c095cff
Luud van Keulen <l.vankeulen@student.fontys.nl>
2016-05-31 10:20:52 +0200

    Added ServerBrowser

---
+++ ServerBrowser/src/queryServer/ServerList.java
@@ -0,0 +1,37 @@
+package queryServer;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerList extends UnicastRemoteObject implements IServerList{
+	public List<IServer> servers;
+	
+	public ServerList () throws  RemoteException {
+		this.servers = new ArrayList();
+	}
+	
+	@Override
+	public List<IServer> getServers(){
+		return this.servers;
+	}
+	
+	@Override
+	public void addServer(String address, int port) throws RemoteException {
+		servers.add(new Server(address, port));
+		System.out.println("added server:" + address + port);
+		for(IServer server : servers) {
+			System.out.println(server.getFullAddress());
+		}
+	}
+	
+	@Override
+	public void removeServer(String address) throws RemoteException {
+		for(IServer s : servers) {
+			if(s.getFullAddress().equals(address)) {
+				servers.remove(s);
+			}
+		}
+	}
+}
